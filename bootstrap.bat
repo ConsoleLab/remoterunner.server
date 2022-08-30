08e2c5f86f03623ab7ca2ca92455f95a53bce507
Hüseyin <iletisim@huseyinyigit.com.tr>
2022-08-30 21:35:10 +0300

    Initial commit.

---
+++ bootstrap.bat
@@ -0,0 +1,22 @@
+@ECHO OFF
+
+MKDIR build
+CD build
+
+IF [%1]==[] GOTO 64BIT
+IF "%1"=="x86" GOTO 32BIT
+
+ECHO Unrecognized building architecture: "%1"
+GOTO EXIT
+
+:64BIT
+cmake.exe -DCMAKE_INSTALL_PREFIX=%USERPROFILE%\AppData\Local ..
+
+:32BIT
+cmake.exe -DCMAKE_INSTALL_PREFIX=%USERPROFILE%\AppData\Local -T host=x86 -A win32 ..
+
+cmake.exe --build . --target INSTALL --config Release
+
+:EXIT
+CD ..
+RMDIR /Q /S build
